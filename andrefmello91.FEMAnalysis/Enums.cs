e2fad5649789e7518fffd4d772bc55a65f59eb92
andremello91 <andremello91@gmail.com>
2021-05-05 18:38:25 -0400

    Moved enums

---
+++ andrefmello91.FEMAnalysis/Enums.cs
@@ -0,0 +1,39 @@
+﻿namespace andrefmello91.FEMAnalysis
+{
+	/// <summary>
+	///     The analysis types.
+	/// </summary>
+	public enum AnalysisType
+	{
+		/// <summary>
+		///     Linear-elastic analysis.
+		/// </summary>
+		Linear,
+
+		/// <summary>
+		///     Nonlinear analysis.
+		/// </summary>
+		Nonlinear
+	}
+
+	/// <summary>
+	///     Nonlinear solution procedures.
+	/// </summary>
+	public enum NonLinearSolver
+	{
+		/// <summary>
+		///     Newton-Raphson nonlinear solver.
+		/// </summary>
+		NewtonRaphson,
+
+		/// <summary>
+		///     Modified Newton-Raphson nonlinear solver.
+		/// </summary>
+		ModifiedNewtonRaphson,
+
+		/// <summary>
+		///     Secant Method nonlinear solver.
+		/// </summary>
+		Secant
+	}
+}
--- andrefmello91.FEMAnalysis/Interfaces.cs
+++ andrefmello91.FEMAnalysis/Interfaces.cs
@@ -6,42 +6,6 @@ using UnitsNet.Units;
 
 namespace andrefmello91.FEMAnalysis
 {
-	/// <summary>
-	///     The analysis types.
-	/// </summary>
-	public enum AnalysisType
-	{
-		/// <summary>
-		///     Linear-elastic analysis.
-		/// </summary>
-		Linear,
-
-		/// <summary>
-		///     Nonlinear analysis.
-		/// </summary>
-		Nonlinear
-	}
-
-	/// <summary>
-	///     Nonlinear solution procedures.
-	/// </summary>
-	public enum NonLinearSolver
-	{
-		/// <summary>
-		///     Newton-Raphson nonlinear solver.
-		/// </summary>
-		NewtonRaphson,
-
-		/// <summary>
-		///     Modified Newton-Raphson nonlinear solver.
-		/// </summary>
-		ModifiedNewtonRaphson,
-
-		/// <summary>
-		///     Secant Method nonlinear solver.
-		/// </summary>
-		Secant
-	}
 
 	/// <summary>
 	///     Interface for numbered elements.
